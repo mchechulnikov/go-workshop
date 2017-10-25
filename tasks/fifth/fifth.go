90d18257fe3295c99268a4aa6ef2cd00a644f8a4
sunloving <ilfirin.nar@gmail.com>
2017-10-25 14:48:33 +0300

    simple tasks: fifth (fix)

--- tasks/fifth/fifth.go
+++ tasks/fifth/fifth.go
@@ -4,28 +4,25 @@ func GetSliceWithMaxSum(values []int) []int {
 	startIndex, endIndex, possibleStartIndex, maxSum := 0, 0, 0, 0
 	sums := make([]int, 0)
 
-	for index, value := range values {
-		if index == 0 {
-			endIndex = 0
-			maxSum = 0
-			if values[0] < 0 {
-				possibleStartIndex = 1
-			} else {
-				maxSum = values[0]
-			}
-			sums = append(sums, maxSum)
-			continue
-		}
+	endIndex = 0
+	maxSum = 0
+	if values[0] < 0 {
+		possibleStartIndex = 1
+	} else {
+		maxSum = values[0]
+	}
+	sums = append(sums, maxSum)
 
-		if value < 0 && sums[index - 1] + value <= 0 {
+	for index, value := range values[1:] {
+		if value < 0 && sums[index] + value <= 0 {
 			value = 0
-			possibleStartIndex = index + 1
+			possibleStartIndex = index + 2
 		}
-		sum := sums[index - 1] + value
+		sum := sums[index] + value
 		if sum >= maxSum {
 			maxSum = sum
 			startIndex = possibleStartIndex
-			endIndex = index
+			endIndex = index + 1
 		}
 		sums = append(sums, sum)
 	}
